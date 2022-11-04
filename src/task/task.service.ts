953589d42050da343bc2d86843a77e306d99f016
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2022-11-04 14:58:46 -0300

    feat: split jobs and adjust times

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -13,9 +13,21 @@ export class TaskService {
     private twitterService: TwitterService,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  // @Cron(CronExpression.EVERY_DAY_AT_8AM)
-  async handleTask() {
+  // @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_DAY_AT_8AM)
+  async handleMorningTask() {
+    try {
+      const tweetFromWeather = await this.fetchWeather();
+      this.twitterService.tweet(tweetFromWeather);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      console.log('finally');
+    }
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_6PM)
+  async handleAfternoonTask() {
     try {
       const tweetFromWeather = await this.fetchWeather();
       this.twitterService.tweet(tweetFromWeather);
